adca45865fbb726c8b4ec710b01d8e701df98cec
Fred Willis <freddiewillis@freds-mbp.home>
2023-08-22 15:31:01 +0100

    First commit

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+// Context object represents a session with the database. The context object allows querying and saving data.
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
